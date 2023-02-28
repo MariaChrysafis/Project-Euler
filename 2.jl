ffb976d8f8a2fa92a856eadad85a1fcd677d058d
Olympia <maria.chrysafis.junior@gmail.com>
2023-02-28 09:30:50 -0800

    Create 2.jl

---
+++ 2.jl
@@ -0,0 +1,20 @@
+[collapse=Julia]
+function f(n)
+    cur = 1
+    prev = 1
+    ans = 0
+    while true
+         if cur > n
+             break
+         end
+         nxt = cur + prev
+         prev = cur
+         cur = nxt
+         if cur % 2 == 0
+             ans += cur
+         end
+    end
+    ans
+end
+println(f(4000000))
+[/collapse]
