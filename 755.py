a737af9642a5c6aa2bd084acab4e97bbd9892511
Olympia <maria.chrysafis.junior@gmail.com>
2023-03-06 18:59:34 -0800

    Create 755.py

---
+++ 755.py
@@ -0,0 +1,20 @@
+import itertools
+
+M = pow(10, 13)
+fib = [1, 2]
+for i in itertools.count(start=2):
+    fib.append(fib[len(fib) - 1] + fib[len(fib) - 2])
+    if fib[i] > M:
+        K = i + 1
+        break
+
+
+def cnt(cur_arr, x):
+    if x < 0:
+        return 0
+    if len(cur_arr) == 0 or sum(cur_arr) <= x:
+        return 1 << len(cur_arr)
+    return cnt(cur_arr[:len(cur_arr)-1], x) + cnt(cur_arr[:len(cur_arr)-1], x - cur_arr[-1])
+
+
+print(cnt(fib, pow(10, 13)))
