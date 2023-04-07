b8c52090e505ce1ad3ad90aed0a001adefa48192
Olympia <maria.chrysafis.junior@gmail.com>
2023-04-07 11:03:00 -0700

    Update 795_test.py

--- 795_test.py
+++ 795_test.py
@@ -5,7 +5,6 @@ def gcd (a, b) :
 MX = 1234
 mu = [0] * (MX + 1)
 isprime = [True] * (MX + 1)
-
 for i in range(2, MX + 1) :
     if isprime[i] :
         mu[i] = 1 - mu[i]
@@ -31,13 +30,11 @@ for i in range(2, MX + 1) :
 
 ans = 0
 for g in range(1, MX + 1) : 
-    for i in range(1, MX + 1) :
-        if i % g != 0 :
-            continue
-        for j in range(1, i + 1) :
+    for i in range(1, MX//g + 1) :
+        for j in range(1, i * g + 1) :
             if (j * j) % g != 0 :
                 continue
-            for g1 in range(1, gcd(i, j * j) + 1) :
-                if ((j * j)//g) % g1 == 0 and (i//g) % g1 == 0 :
+            for g1 in range(1, gcd(i * g, j * j) + 1) :
+                if ((j * j)//g) % g1 == 0 and i % g1 == 0 :
                     ans += g * mu[g1] * pow(-1, j)
 print(ans)
